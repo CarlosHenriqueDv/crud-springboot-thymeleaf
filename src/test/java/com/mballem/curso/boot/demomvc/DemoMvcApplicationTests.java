03be14701a629edeafeae574a1703919869c6fc8
Carlos Henrique <cmendonca@atentatecnologia.com.br>
2020-05-18 14:19:38 -0300

    Initial commit

---
+++ src/test/java/com/mballem/curso/boot/demomvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.boot.demomvc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
