0851639c69ab801d3ceb3849b9a0f086210062a3
Carlos Henrique <cmendonca@atentatecnologia.com.br>
2020-05-18 16:36:58 -0300

    novas implementações

---
+++ src/main/java/com/mballem/curso/boot/demomvc/domain/Cargo.java
@@ -0,0 +1,31 @@
+package com.mballem.curso.boot.demomvc.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CARGOS")
+public class Cargo extends AbstractEntity<Long> {
+
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_departamento_fk")
+    private Departamento departamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
